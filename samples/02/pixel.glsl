8a5b13d9d34e51aaeb05cd71863388ed1674a1a3
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2017-08-30 23:37:48 +0200

    Render border rectangle

--- samples/02/pixel.glsl
+++ samples/02/pixel.glsl
@@ -11,8 +11,15 @@ uniform float u_time;
 void main() {
   vec2 st = gl_FragCoord.xy / u_resolution;
 
-  float y = step(0.5 + 0.5 * sin(2.0 * PI * st.x), st.y);
-  vec3 color = vec3(y);
+  // bottom left
+  vec2 bl = step(vec2(0.1), st);
+  float pct = bl.x * bl.y;
+
+  // top right
+  vec2 tr = step(vec2(0.1), 1.0 - st);
+  pct *= tr.x * tr.y;
+
+  vec3 color = vec3(pct);
 
   gl_FragColor = vec4(color, 1.0);
 }
