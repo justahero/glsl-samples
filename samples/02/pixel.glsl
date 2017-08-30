67ff165a8cc9415f587e1293e74c8fbb6d947191
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2017-08-30 23:20:53 +0200

    Add second sample

---
+++ samples/02/pixel.glsl
@@ -0,0 +1,18 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+#define PI 3.14159265359
+
+uniform vec2 u_resolution;
+uniform vec2 u_mouse;
+uniform float u_time;
+
+void main() {
+  vec2 st = gl_FragCoord.xy / u_resolution;
+
+  float y = step(0.5 + 0.5 * sin(2.0 * PI * st.x), st.y);
+  vec3 color = vec3(y);
+
+  gl_FragColor = vec4(color, 1.0);
+}
