903f1370deda11dffb6bb398f4b3b69db74b9abf
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2017-08-30 22:47:20 +0200

    Refine shader

--- samples/01/pixel.glsl
+++ samples/01/pixel.glsl
@@ -2,16 +2,17 @@
 precision mediump float;
 #endif
 
+#define PI 3.14159265359
+
 uniform vec2 u_resolution;
+uniform vec2 u_mouse;
+uniform float u_time;
 
 void main() {
-  // start off with black
-  vec3 color = vec3(0.);
-  // normalise coords to be from 0 - 1
-  vec2 st = gl_FragCoord.xy/u_resolution;
-  // if pixel is beyond halfway point, assign 1 value to color (which is white)
-  // otherwise, back to 0 which is black
-  color += step(.5,st.x);
-  // set the color and move on
-  gl_FragColor = vec4(color,1.);
+  vec2 st = gl_FragCoord.xy / u_resolution;
+
+  float y = step(0.5 + 0.5 * sin(2.0 * PI * st.x), st.y);
+  vec3 color = vec3(y);
+
+  gl_FragColor = vec4(color, 1.0);
 }
