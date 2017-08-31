0c6068d1646c7a1e742c5365403e673ae02179b3
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2017-08-31 22:22:06 +0200

    Extract drawing of rectangle to function

--- samples/03/pixel.glsl
+++ samples/03/pixel.glsl
@@ -11,18 +11,16 @@ uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 uniform float u_time;
 
+float rectangle(vec2 st, float width, float radius) {
+  vec2 bl = smoothstep(vec2(width), st, vec2(radius));
+  vec2 tr = smoothstep(vec2(width), 1.0 - st, vec2(radius));
+  return bl.x * bl.y * tr.x * tr.y;
+}
+
 void main() {
   vec2 st = gl_FragCoord.xy / u_resolution;
 
-  // bottom left
-  float width = 0.02;
-  vec2 bl = smoothstep(vec2(width), st, vec2(0.5));
-  float pct = bl.x * bl.y;
-
-  // top right
-  vec2 tr = smoothstep(vec2(width), 1.0 - st, vec2(0.5));
-  pct *= tr.x * tr.y;
-
+  float pct = rectangle(st, 0.02, 0.5);
   vec3 color = vec3(pct);
 
   gl_FragColor = vec4(color, 1.0);
