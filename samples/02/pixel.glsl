559085cd62b45f9a68a9dd348b8b074c37fc0350
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2017-09-01 23:32:58 +0200

    Refactor function to calculate bordered rectangle

--- samples/02/pixel.glsl
+++ samples/02/pixel.glsl
@@ -8,37 +8,37 @@ uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 uniform float u_time;
 
-float drawRect(vec2 st) {
-  float left   = 1.0;
-  float right  = 1.0;
-  float top    = 1.0;
-  float bottom = 1.0;
+float drawRect(vec2 st, float left, float top, float right, float bottom, float width) {
+  float l = 1.0;
+  float r = 1.0;
+  float t = 1.0;
+  float b = 1.0;
 
-  if (0.2 <= st.y && st.y <= 0.7) {
-    if (0.2 <= st.x && st.x <= 0.3) {
-      left = 0.0;
+  if (top <= st.y && st.y <= bottom) {
+    if (left <= st.x && st.x <= left + width) {
+      l = 0.0;
     }
-    if (0.6 <= st.x && st.x <= 0.7) {
-      right = 0.0;
+    if (right - width <= st.x && st.x <= right) {
+      r = 0.0;
     }
   }
 
-  if (0.2 <= st.x && st.x <= 0.7) {
-    if (0.2 <= st.y && st.y <= 0.3) {
-      top = 0.0;
+  if (left <= st.x && st.x <= right) {
+    if (top - width <= st.y && st.y <= top) {
+      t = 0.0;
     }
-    if (0.6 <= st.y && st.y <= 0.7) {
-      bottom = 0.0;
+    if (bottom - width <= st.y && st.y <= bottom) {
+      b = 0.0;
     }
   }
 
-  return (left * top) * (right * bottom);
+  return (l * t) * (r * b);
 }
 
 void main() {
   vec2 st = gl_FragCoord.xy / u_resolution;
 
-  float pct = drawRect(st);
+  float pct = drawRect(st, 0.20, 0.25, 0.60, 0.90, 0.01);
   vec3 color = vec3(pct);
 
   gl_FragColor = vec4(color, 1.0);
