91460a9a0d3976676402d5125cf84687476e0d77
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2017-09-01 23:29:23 +0200

    Render rectangle

--- samples/02/pixel.glsl
+++ samples/02/pixel.glsl
@@ -8,17 +8,37 @@ uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 uniform float u_time;
 
+float drawRect(vec2 st) {
+  float left   = 1.0;
+  float right  = 1.0;
+  float top    = 1.0;
+  float bottom = 1.0;
+
+  if (0.2 <= st.y && st.y <= 0.7) {
+    if (0.2 <= st.x && st.x <= 0.3) {
+      left = 0.0;
+    }
+    if (0.6 <= st.x && st.x <= 0.7) {
+      right = 0.0;
+    }
+  }
+
+  if (0.2 <= st.x && st.x <= 0.7) {
+    if (0.2 <= st.y && st.y <= 0.3) {
+      top = 0.0;
+    }
+    if (0.6 <= st.y && st.y <= 0.7) {
+      bottom = 0.0;
+    }
+  }
+
+  return (left * top) * (right * bottom);
+}
+
 void main() {
   vec2 st = gl_FragCoord.xy / u_resolution;
 
-  // bottom left
-  vec2 bl = step(vec2(0.1), st);
-  float pct = bl.x * bl.y;
-
-  // top right
-  vec2 tr = step(vec2(0.1), 1.0 - st);
-  pct *= tr.x * tr.y;
-
+  float pct = drawRect(st);
   vec3 color = vec3(pct);
 
   gl_FragColor = vec4(color, 1.0);
