f19712563343b0196057f713a150cfb422d6bffe
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2017-09-03 19:44:01 +0200

    Change sample to animated circle

--- samples/04/pixel.glsl
+++ samples/04/pixel.glsl
@@ -12,7 +12,7 @@ void main() {
   vec2 st = gl_FragCoord.xy / u_resolution;
 
   vec2 toCenter = vec2(0.5) - st;
-  float pct = step(0.5, length(toCenter) * 2.0);
+  float pct = smoothstep(0.5, length(toCenter) * 2.0, 0.50 + 0.10 * sin(u_time * 4.0));
   vec3 color = vec3(1.0 - pct, 0.0, 0.0);
 
   gl_FragColor = vec4(color, 1.0);
