d79834794046e7cc867335540d020252b69d66a8
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2017-09-03 19:58:05 +0200

    Refactor function

--- samples/04/pixel.glsl
+++ samples/04/pixel.glsl
@@ -8,17 +8,17 @@ uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 uniform float u_time;
 
-float circle(vec2 pos, float radius, float smooth) {
+float circle(vec2 size, vec2 pos, float radius, float smooth) {
   float hello = radius - 0.02 * sin(u_time * 2.5);
-  float pct = smoothstep(hello, length(pos), radius + smooth * sin(u_time * 2.5));
+  float pct = smoothstep(radius, length(pos - size), radius + smooth * sin(u_time * 2.5));
   return pct;
 }
 
 void main() {
   vec2 st = gl_FragCoord.xy / u_resolution;
 
-  float pct = circle(vec2(0.3) - st, 0.25, 0.03);
-  pct += circle(vec2(0.8, 0.8) - st, 0.15, 0.015);
+  float pct = 0.0;
+  pct = circle(st, vec2(0.7, 0.7), 0.25, 0.03) + circle(st, vec2(0.18), 0.15, 0.015);
 
   vec3 color = vec3(1.0 - pct, 0.0, 0.0);
 
