4ee7a14cc6965549083c2a14b6312fcde387d732
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2017-09-03 19:49:31 +0200

    Refactor circle logic to its own function

--- samples/04/pixel.glsl
+++ samples/04/pixel.glsl
@@ -8,11 +8,18 @@ uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 uniform float u_time;
 
+float circle(vec2 pos, float radius, float smooth) {
+  float hello = radius - 0.02 * sin(u_time * 2.5);
+  float pct = smoothstep(hello, length(pos), radius + smooth * sin(u_time * 2.5));
+  return pct;
+}
+
 void main() {
   vec2 st = gl_FragCoord.xy / u_resolution;
 
-  vec2 toCenter = vec2(0.5) - st;
-  float pct = smoothstep(0.5, length(toCenter) * 2.0, 0.50 + 0.10 * sin(u_time * 4.0));
+  float pct = circle(vec2(0.3) - st, 0.25, 0.03);
+  pct += circle(vec2(0.8, 0.8) - st, 0.15, 0.015);
+
   vec3 color = vec3(1.0 - pct, 0.0, 0.0);
 
   gl_FragColor = vec4(color, 1.0);
