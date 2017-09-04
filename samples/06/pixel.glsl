652c7b63deab795dc16861e1d1e523cb7523392d
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2017-09-05 00:09:53 +0200

    Adjust sample to render flower like effect

--- samples/06/pixel.glsl
+++ samples/06/pixel.glsl
@@ -8,28 +8,20 @@ uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 uniform float u_time;
 
-float distance(in vec2 st, in vec2 pos, float radius) {
-  vec2 dist = st - pos;
-  return 1.0 - smoothstep(radius - (radius * 0.01),
-                          radius + (radius * 0.01),
-                          dot(dist, dist) * 4.0);
-}
-
 void main() {
   vec2 st = gl_FragCoord.xy / u_resolution;
 
-  // adjust for aspect ratio
-  st.x *= u_resolution.x / u_resolution.y;
-  st = st * 2.0 - 1.0;
-
+  vec2 pos = vec2(0.5) - st;
   float d = 0.0;
-  d = length(abs(st) - (0.25 + 0.05 * sin(u_time * 4.0) + 0.10 * cos(u_time * 2.0)));
-  // d = distance(st, vec2(0.6, 0.50), 0.5) * distance(st, vec2(0.4, 0.75), 0.4);
-  // d = min(distance(st, vec2(0.6, 0.50), 0.5), distance(st, vec2(0.4, 0.75), 0.4));
-  // d = max(distance(st, vec2(0.6, 0.50), 0.5), distance(st, vec2(0.4, 0.75), 0.4));
-  // d = pow(distance(st, vec2(0.50, 0.50), 0.45), distance(st, vec2(0.40, 0.60), 0.4));
 
-  vec3 color = vec3(fract(15.0 * d), 0.0, 0.4 + 0.25 * tan(u_time));
+  float r = length(pos) * 2.2;
+  float a = atan(pos.y, pos.x);
+
+  // determine value
+  // d = cos(a * 3.0);
+  d = abs(cos(a * 4.0));
+
+  vec3 color = vec3(1.0 - smoothstep(d, d + 0.1, r));
 
   gl_FragColor = vec4(color, 1.0);
 }
