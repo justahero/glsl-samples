f2049727fa8eb54c7014098d75a1179725cf7c3b
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2017-09-07 21:56:33 +0200

    Extract gear shape logic into function

--- samples/07/pixel.glsl
+++ samples/07/pixel.glsl
@@ -8,6 +8,11 @@ uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 uniform float u_time;
 
+float gear(vec2 pos, float bumps, float radius) {
+  float a = atan(pos.y, pos.x);
+  return smoothstep(-0.50, 1.0, 1.2 * cos(a * bumps)) * (radius * 0.25) + radius;
+}
+
 mat2 rotate(float angle) {
   return mat2(cos(angle), -sin(angle),
               sin(angle),  cos(angle));
@@ -17,17 +22,14 @@ void main() {
   vec2 st = gl_FragCoord.xy / u_resolution;
 
   st -= vec2(0.5);
-  st = rotate(u_time * PI * 0.2) * st;
+  st *= rotate(u_time * PI * 0.25);
 
   vec2 pos = st;
 
-  float r = length(pos) * 2.2;
-  float a = atan(pos.y, pos.x);
+  float l = length(pos);
+  float f = gear(pos, 14.0, 0.72);
 
-  float f = 0.0;
-  f = smoothstep(-0.75, 1.0, cos(a * 10.0)) * 0.15 + 0.60;
-
-  vec3 color = vec3(1.0 - smoothstep(f, f + 0.02, r));
+  vec3 color = vec3(1.0 - smoothstep(f, f + 0.02, l * 2.2));
 
   gl_FragColor = vec4(color, 1.0);
 }
