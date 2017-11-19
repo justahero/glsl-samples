53c025b19fe441d5b850e0cc5f370d3aff2fecc2
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2017-11-19 18:26:36 +0100

    Add pixel spirit card Justice

---
+++ pixelspirit/02/pixel.glsl
@@ -0,0 +1,15 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform vec2 u_resolution;
+uniform float u_time;
+
+void main() {
+  vec2 st = gl_FragCoord.xy / u_resolution;
+
+  vec3 color = vec3(0.0);
+  color += step(0.5, st.x);
+
+  gl_FragColor = vec4(color, 1.0);
+}
