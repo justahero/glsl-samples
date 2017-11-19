48b782d3518e5da1bf45abfc17c885f850fb3c08
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2017-11-19 22:51:18 +0100

    Add pixelspirit card 8 The Hanged Man

---
+++ pixelspirit/08/pixel.glsl
@@ -0,0 +1,28 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+#define PI 3.14159265359
+
+uniform vec2 u_resolution;
+uniform float u_time;
+
+float stroke(float x, float s, float w) {
+  float d = step(s, x + w * 0.5) - step(s, x - w * 0.5);
+  return clamp(d, 0.0, 1.0);
+}
+
+void main() {
+  vec2 st = gl_FragCoord.xy / u_resolution.x;
+  vec3 color = vec3(0.0);
+
+  float offset = 0.3;
+
+  float sdf = 0.5 + (st.x - st.y + offset) * 0.5;
+  color += stroke(sdf, 0.5, 0.1);
+
+  float sdf_inv = (st.x + st.y - offset) * 0.5;
+  color += stroke(sdf_inv, 0.5, 0.1);
+
+  gl_FragColor = vec4(color, 1.0);
+}
