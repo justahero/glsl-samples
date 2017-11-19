48873169dac01652721a75ddc4879afff96c328e
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2017-11-19 22:13:32 +0100

    Add pixel spirit card Strength

---
+++ pixelspirit/03/pixel.glsl
@@ -0,0 +1,17 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+#define PI 3.14159265359
+
+uniform vec2 u_resolution;
+uniform float u_time;
+
+void main() {
+  vec2 st = gl_FragCoord.xy / u_resolution;
+
+  vec3 color = vec3(0.0);
+  color += step(0.5 + cos(st.y * PI) * 0.25, st.x);
+
+  gl_FragColor = vec4(color, 1.0);
+}
