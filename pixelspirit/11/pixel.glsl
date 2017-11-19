2a06e007d2b82616fda7f02198205504d3a4a1fc
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2017-11-19 23:17:36 +0100

    Add pixelspirit card 11 The Emperor

---
+++ pixelspirit/11/pixel.glsl
@@ -0,0 +1,40 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+#define PI 3.14159265359
+
+uniform vec2 u_resolution;
+uniform float u_time;
+
+float stroke(float x, float s, float w) {
+  float d = step(s, x + w * 0.5) - step(s, x - w * 0.5);
+  return clamp(d, 0.0, 1.0);
+}
+
+float circleSDF(vec2 st) {
+  return length(st - 0.5) * 2.0;
+}
+
+float rectSDF(vec2 st, vec2 size) {
+  st = st * 2.0 - 1.0;
+  return max(abs(st.x / size.x), abs(st.y / size.y));
+}
+
+float fill(float x, float size) {
+  return 1.0 - step(size, x);
+}
+
+void main() {
+  vec2 st = (gl_FragCoord.xy / u_resolution);
+  st.y *= (u_resolution.y / u_resolution.x);
+  st.y -= 0.25;
+
+  vec3 color = vec3(0.0);
+
+  float sdf = rectSDF(st, vec2(1.0));
+  color += stroke(sdf, 0.5, 0.125);
+  color += fill(sdf, 0.1);
+
+  gl_FragColor = vec4(color, 1.0);
+}
