9c0e67d6e0972081f1565da2de796b3e8b41c9e3
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2025-01-26 14:26:48 +0100

    Update

--- samples/post/vignette/pixel.glsl
+++ samples/post/vignette/pixel.glsl
@@ -9,11 +9,11 @@ uniform vec2 u_mouse;
 uniform float u_time;
 
 const float RADIUS   = 0.75;
-const float SOFTNESS = 0.3;
-const float ALPHA    = 0.15;
+const float SOFTNESS = 0.25;
+const float ALPHA    = 0.45;
 
 const vec3 COLOR1 = vec3(1.0);
-const vec3 COLOR2 = vec3(0.85, 0.9, 1.0);
+const vec3 COLOR2 = vec3(0.95, 0.95, 0.95);
 
 void main() {
   vec2 st = gl_FragCoord.xy / u_resolution.xy;
